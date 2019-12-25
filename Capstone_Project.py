a73edcb678eb4a240f3dc91ae39151386a91f2a4
ryan_diaz_perez <ryan.diazperez@colorado.edu>
2019-12-24 17:35:58 -0800

    Part 1 of week 3 python files

---
+++ Capstone_Project.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This Jupyter Notebook is for the IBM Data Science Capstone
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
