1c300f66d529c1b8b828ee8a48119a862270f7bd
ryan_diaz_perez <ryan.diazperez@colorado.edu>
2019-12-25 15:53:08 -0800

    Part 2 of week 3 assignment

--- Toronto_Clustering.py
+++ Toronto_Clustering.py
@@ -5,27 +5,39 @@
 
 # <h1 align=left><font size = 4>Part 1: Web Scraping</font></h1>
 
-# In[14]:
+# In[1]:
+
+
+get_ipython().system('conda install lxml --yes')
+get_ipython().system('conda install html5lib --yes')
+get_ipython().system('conda install -c conda-forge beautifulsoup4 --yes')
+
+
+# In[2]:
 
 
 import numpy as np # library to handle data in a vectorized manner
 import pandas as pd # library for data analsysis
 from sklearn.cluster import KMeans # import k-means from clustering stage
+import html5lib
+from bs4 import BeautifulSoup
+import lxml
 
 
 # <Big><b>Extracting data from the Wikipedia page</b></Big>
 
-# In[15]:
+# In[3]:
 
 
-df_toronto = pd.read_html("https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M", flavor="html5lib")[0]
+url = "http://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M"
+df_toronto = pd.read_html(url, flavor='html5lib')[0]
 
 # Removing rows with Borough value of Not assigned
 df_toronto.drop(df_toronto.index[df_toronto['Borough'] == 'Not assigned'], inplace = True)
 df_toronto.head()
 
 
-# In[16]:
+# In[4]:
 
 
 # Combining rows
@@ -34,14 +46,14 @@ df_toronto.reset_index(level=['Postcode','Borough'], inplace=True)
 df_toronto.head(210)
 
 
-# In[17]:
+# In[5]:
 
 
 df_toronto.loc[df_toronto['Neighbourhood'] == ('Not assigned'), 'Neighbourhood'] = df_toronto['Borough']
 df_toronto.head(90)
 
 
-# In[18]:
+# In[6]:
 
 
 df_toronto.shape
@@ -49,22 +61,29 @@ df_toronto.shape
 
 # <h1 align=left><font size = 4>Part 2: Geographical locations</font></h1>
 
-# In[ ]:
+# In[25]:
 
 
+# Latitude and Longitude of the neighborhoods
+df_geography = pd.read_csv('Geospatial_Coordinates.csv')
+df_geography.head()
 
 
-
-# In[ ]:
-
+# In[26]:
 
 
+# Renaming colunm from Postal Code to Postcode
+df_geography.rename({'Postal Code': 'Postcode'}, axis=1, inplace=True)
+df_geography.head()
 
 
-# In[ ]:
+# In[27]:
 
 
-
+# Merging df_toronto and df_geography
+df_merged = df_toronto.merge(df_geography, how='left', on='Postcode')
+df_merged.rename({'Postcode': 'PostalCode'}, axis=1, inplace=True)
+df_merged.head()
 
 
 # <h1 align=left><font size = 4>Part 3: Maps of the neighborhood</font></h1>
